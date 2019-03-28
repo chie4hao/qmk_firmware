57e90143b726148dcc1a636f36b0fe2270fa59a0
chie4hao <w310930920@gmail.com>
2019-03-28 20:45:23 +0800

    add usb_usb files

---
+++ keyboards/converter/usb_usb/keymaps/chie4/config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 250
+
+#define PERMISSIVE_HOLD
+#define TAPPING_FORCE_HOLD
+#define ONESHOT_TIMEOUT 300
+
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#endif
