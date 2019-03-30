7e3fa4ff3352a92f817d2ff155cdcd1cc51083bd
chie4hao <w310930920@gmail.com>
2019-03-31 01:24:04 +0800

    Change crkbd keymap

--- keyboards/crkbd/keymaps/chie4/keymap.c
+++ keyboards/crkbd/keymaps/chie4/keymap.c
@@ -58,75 +58,35 @@ enum macro_keycodes {
 #define KC_ALTKN ALT_T(KC_LANG1)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_kc( \
-  //,-----------------------------------------.                ,-----------------------------------------.
-        ESC,     Q,     W,     E,     R,     T,                      Y,     U,     I,     O,     P,  BSPC,\
-  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-      CTLTB,     A,     S,     D,     F,     G,                      H,     J,     K,     L,  SCLN,  QUOT,\
-  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-       LSFT,     Z,     X,     C,     V,     B,                      N,     M,  COMM,   DOT,  SLSH,  RSFT,\
-  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                                  GUIEI, LOWER,   SPC,      ENT, RAISE, ALTKN \
-                              //`--------------------'  `--------------------'
-  ),
-
-  [1] = LAYOUT_kc( \
-  //,-----------------------------------------.                ,-----------------------------------------.
-        ESC,     1,     2,     3,     4,     5,                      6,     7,     8,     9,     0,  BSPC,\
-  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-      CTLTB,    F1,    F2,    F3,    F4,    F5,                     F6,    F7,    F8,    F9,   F10, XXXXX,\
-  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-       LSFT,   F11,   F12,   F13,   F14,   F15,                    F16,   F17,   F18,   F19,   F20, XXXXX,\
-  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                                  GUIEI, LOWER,   SPC,      ENT, RAISE, ALTKN \
-                              //`--------------------'  `--------------------'
-  ),
-
-  [2] = LAYOUT_kc( \
-  //,-----------------------------------------.                ,-----------------------------------------.
-        ESC,  EXLM,    AT,  HASH,   DLR,  PERC,                   CIRC,  AMPR,  ASTR,  LPRN,  RPRN,  BSPC,\
-  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-      CTLTB, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,                   MINS,   EQL,  LCBR,  RCBR,  PIPE,   GRV,\
-  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-       LSFT, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,                   UNDS,  PLUS,  LBRC,  RBRC,  BSLS,  TILD,\
-  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                                  GUIEI, LOWER,   SPC,      ENT, RAISE, ALTKN \
-                              //`--------------------'  `--------------------'
-  ),
-
-  [3] = LAYOUT_kc( \
-  //,-----------------------------------------.                ,-----------------------------------------.
-        RST,  LRST, XXXXX, XXXXX, XXXXX, XXXXX,                  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
-  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-       LTOG,  LHUI,  LSAI,  LVAI, XXXXX, XXXXX,                  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
-  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-       LMOD,  LHUD,  LSAD,  LVAD, XXXXX, XXXXX,                  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
-  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                                  GUIEI, LOWER,   SPC,      ENT, RAISE, ALTKN \
-                              //`--------------------'  `--------------------'
-                   ),
-  [4] = LAYOUT_kc( \
-                  //,-----------------------------------------.                ,-----------------------------------------.
-                  RST,  LRST, XXXXX, XXXXX, XXXXX, XXXXX,                  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
-                  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-                  LTOG,  LHUI,  LSAI,  LVAI, XXXXX, XXXXX,                  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
-                  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-                  LMOD,  LHUD,  LSAD,  LVAD, XXXXX, XXXXX,                  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
-                  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                  GUIEI, LOWER,   SPC,      ENT, RAISE, ALTKN \
-                  //`--------------------'  `--------------------'
-                   ),
-  [5] = LAYOUT_kc( \
-                  //,-----------------------------------------.                ,-----------------------------------------.
-                  RST,  LRST, XXXXX, XXXXX, XXXXX, XXXXX,                  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
-                  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-                  LTOG,  LHUI,  LSAI,  LVAI, XXXXX, XXXXX,                  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
-                  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-                  LMOD,  LHUD,  LSAD,  LVAD, XXXXX, XXXXX,                  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
-                  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                  GUIEI, LOWER,   SPC,      ENT, RAISE, ALTKN \
-                  //`--------------------'  `--------------------'
-                   ),
+    LAYOUT( //  default layer
+                 KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_RPRN,
+                 MT(MOD_LCTL,KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LT(2,KC_SCLN), MT(MOD_RCTL,KC_QUOT),
+                 KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, LT(1,KC_SLSH), KC_SFTENT,
+                 KC_LALT, KC_LGUI, OSM(MOD_LSFT), LT(3,KC_SPC), MO(4), KC_RGUI),
+    // slash
+    LAYOUT(
+                 KC_TRNS, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_NO  , KC_TRNS,
+                 KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, KC_F10 , KC_TRNS, KC_APP , KC_TRNS, KC_TRNS, KC_NO  , KC_TRNS,
+                 KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_F11 , KC_F12 , KC_INS , KC_PSCR, KC_SLCK, KC_PAUS, KC_NO  , KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,          KC_RALT, KC_RGUI),
+    // scolon
+    LAYOUT(
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSPC, KC_DEL , KC_NO  , KC_NO  , KC_NO,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_NO  , KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END , KC_NO  , KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,          KC_RALT, KC_RGUI),
+    // space
+    LAYOUT(
+                 KC_GRV , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LBRC, KC_RBRC, KC_TRNS,
+                 KC_TRNS, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSLS,
+                 KC_TRNS, KC_TILD, KC_PIPE, KC_LCBR, KC_RCBR, KC_UNDS, KC_PLUS, KC_MINS, KC_EQL , KC_DOT , KC_SLSH, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,          KC_RALT, KC_RGUI),
+    // mouse
+    LAYOUT(
+                RGB_MOD ,RGB_RMOD,RGB_M_P ,RGB_M_B ,RGB_M_R ,RGB_M_SW,RGB_M_SN, KC_WH_D, KC_MS_U, KC_WH_U, KC_NO  , KC_NO  ,
+                 KC_LCTL,RGB_M_K ,RGB_M_X ,RGB_M_G ,RGB_M_T ,RGB_TOG , KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN1, KC_TRNS,
+                 KC_TRNS,RGB_HUI ,RGB_HUD ,RGB_SAI ,RGB_SAD ,RGB_VAI ,RGB_VAD , KC_ACL0, KC_ACL1, KC_ACL2, KC_NO  , KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,          KC_RALT, KC_RGUI),
 };
 
 int RGB_current_mode;
