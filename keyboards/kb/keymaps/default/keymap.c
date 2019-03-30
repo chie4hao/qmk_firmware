eabac9cd8cc5ecb1f1163ff8cd778593b27a3a62
chie4hao <w310930920@gmail.com>
2019-03-31 00:02:01 +0800

    Change my keymap

--- keyboards/kb/keymaps/default/keymap.c
+++ keyboards/kb/keymaps/default/keymap.c
@@ -4,9 +4,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP( //  default layer
-                 KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_RBRC,
+                 KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_RPRN,
                  MT(MOD_LCTL,KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LT(2,KC_SCLN), MT(MOD_RCTL,KC_QUOT),
-                 MT(MOD_LSFT,KC_LBRC), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, LT(1,KC_SLSH), SFTENT,
+                 KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, LT(1,KC_SLSH), SFTENT,
                  KC_NO,   KC_NO,   KC_LALT, KC_LGUI, OSM(MOD_LSFT), LT(3,KC_SPC), MO(4), KC_RGUI,KC_NO,KC_NO),
     // slash
     KEYMAP(
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,          KC_RALT, KC_RGUI, KC_NO  , KC_NO),
     // space
     KEYMAP(
-                 KC_GRV , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
+                 KC_GRV , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LBRC, KC_RBRC, KC_TRNS,
                  KC_TRNS, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSLS,
                  KC_TRNS, KC_TILD, KC_PIPE, KC_LCBR, KC_RCBR, KC_UNDS, KC_PLUS, KC_MINS, KC_EQL , KC_DOT , KC_SLSH, KC_TRNS,
                  KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,          KC_RALT, KC_RGUI, KC_NO  , KC_NO),
